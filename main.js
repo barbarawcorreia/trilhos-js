dcbe1976206fbcbdc2ad33efedba14139075797b
Bárbara Correia <barbaracorreia@MacBook-Pro-de-Barbara.local>
2019-02-27 20:54:37 -0300

    WIP

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 
-function vagoes_lifo(valores_entrada){
-// var valores_saida = new Array();
+module.exports.vagoes_lifo = function vagoes_lifo(valores_entrada){
 var valores_saida = []
 
 for (index = valores_entrada.length - 1; index >= 0; index--) {
-    // var valores_saida = []
     valores_saida.push(valores_entrada[index])
 }
 
 return valores_saida
 }
 
-module.exports =  vagoes_lifo
+module.exports.vagoes_fifo = function vagoes_fifo(valores_entrada){
+var valores_saida = []
+
+for (index = 0; index < valores_entrada.length; index++) {
+    valores_saida.push(valores_entrada[index])
+}
+
+return valores_saida
+}
+
+
+// module.exports =  {vagoes_lifo, vagoes_fifo}
 
 //https://medium.com/trainingcenter/entendendo-m%C3%B3dulos-no-javascript-73bce1d64dbf
--- testes.test.js
+++ testes.test.js
@@ -1,12 +1,20 @@
-const vagoes_lifo = require('./main')
+// const vagoes_lifo = require('./main')
+const vagoes = require('./main');
 
 test('LIFO',() => {
     var valores_entrada_cte = [1,2,3,4,5]
     var valor_da_saida_esperado = [5,4,3,2,1]
-    console.log(vagoes_lifo(valores_entrada_cte))
-    expect(vagoes_lifo(valores_entrada_cte)).toEqual(valor_da_saida_esperado)
+    expect(vagoes.vagoes_lifo(valores_entrada_cte)).toEqual(valor_da_saida_esperado)
 })
 
 
-// continuar
 //https://medium.com/trainingcenter/uma-vis%C3%A3o-geral-de-testes-em-javascript-em-2018-8484154caf63
+
+test('FIFO',() => {
+    var valores_entrada_cte = [1,2,3,4,5]
+    var valor_da_saida_esperado = [1,2,3,4,5]
+    console.log(vagoes.vagoes_fifo(valores_entrada_cte))
+    expect(vagoes.vagoes_fifo(valores_entrada_cte)).toEqual(valor_da_saida_esperado)
+})
+
+// cobrir o outro cenário in golang e passar pra cá.
