2f9e1a6f484a8290b7108261ca128ad687241d65
Bárbara Correia <barbara.correia@corp.globo.com>
2019-02-27 11:11:06 -0300

    WIP

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 
 function vagoes_lifo(valores_entrada){
+// var valores_saida = new Array();
 var valores_saida = []
 
-for (index = valoresEntrada.lenght - 1; index >= 0; index--) {
-    valores_saida.push(valores_saida, valores_entrada[index])
+for (index = valores_entrada.length - 1; index >= 0; index--) {
+    // var valores_saida = []
+    valores_saida.push(valores_entrada[index])
 }
 
 return valores_saida
 }
+
+module.exports =  vagoes_lifo
+
+//https://medium.com/trainingcenter/entendendo-m%C3%B3dulos-no-javascript-73bce1d64dbf
