0c419ec96eb1b86420ef69e029da25bb507106a5
Bárbara Correia <barbara.correia@corp.globo.com>
2019-02-27 10:09:19 -0300

    WIP

---
+++ testes.test.js
@@ -0,0 +1,9 @@
+const trilhos = require('./main')
+
+test('LIFO',() => {
+    expect(trilhos()).toEqual()
+})
+
+
+// continuar
+//https://medium.com/trainingcenter/uma-vis%C3%A3o-geral-de-testes-em-javascript-em-2018-8484154caf63
